6aea69e1af889091e535df3b5f127163124b5892
Tíchh Giỏi <tichgioi2003@gmail.com>
2023-05-11 15:00:54 +0700

    12

--- đồ án 2/SVDemo/staff.js
+++ đồ án 2/SVDemo/staff.js
@@ -2,6 +2,8 @@ function emailIsValid(email) {
     return /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/.test(email)
 }
 
+let currentEditingStaffIndex = null;
+
 function addNew() {
     let fullname = document.getElementById('fullname').value;
     let email = document.getElementById('email').value;
@@ -9,75 +11,89 @@ function addNew() {
     let address = document.getElementById('address').value;
     let gender = '';
 
-        if (document.getElementById('male').checked) {
-            gender = document.getElementById('male').value;
-        }else if (document.getElementById('female').checked) {
-            gender = document.getElementById('female').value;
-        }
+    if (document.getElementById('male').checked) {
+        gender = document.getElementById('male').value;
+    }else if (document.getElementById('female').checked) {
+        gender = document.getElementById('female').value;
+    }
 
-        if (_.isEmpty(fullname)) {
-            fullname = '';
-            document.getElementById('fullname-error').innerHTML = 'Vui lòng nhập Họ và tên';
-        }else if(fullname.trim().length <= 2) {
-            fullname = '';
-            document.getElementById('fullname-error').innerHTML = 'Không được nhỏ hơn 2 ký tự';
-        }else if(fullname.trim().length > 50) {
-            fullname = '';
-            document.getElementById('fullname-error').innerHTML = 'Không được lớn hơn 50 ký tự';
-        }else {
-            document.getElementById('fullname-error').innerHTML = '';
-        }
+    if (_.isEmpty(fullname)) {
+        fullname = '';
+        document.getElementById('fullname-error').innerHTML = 'Vui lòng nhập Họ và tên';
+    }else if(fullname.trim().length <= 2) {
+        fullname = '';
+        document.getElementById('fullname-error').innerHTML = 'Không được nhỏ hơn 2 ký tự';
+    }else if(fullname.trim().length > 50) {
+        fullname = '';
+        document.getElementById('fullname-error').innerHTML = 'Không được lớn hơn 50 ký tự';
+    }else {
+        document.getElementById('fullname-error').innerHTML = '';
+    }
 
-        if (_.isEmpty(email)) {
-            email = '';
-            document.getElementById('email-error').innerHTML = 'Vui lòng nhập email của bạn';
-        }else if (!emailIsValid(email)) {
-            email = '';
-            document.getElementById('email-error').innerHTML = 'Email không đúng định dạng';
-        }else {
-            document.getElementById('email-error').innerHTML = '';
-        }
+    if (_.isEmpty(email)) {
+        email = '';
+        document.getElementById('email-error').innerHTML = 'Vui lòng nhập email của bạn';
+    }else if (!emailIsValid(email)) {
+        email = '';
+        document.getElementById('email-error').innerHTML = 'Email không đúng định dạng';
+    }else {
+        document.getElementById('email-error').innerHTML = '';
+    }
 
-        if (_.isEmpty(phone)) {
-            phone = '';
-            document.getElementById('phone-error').innerHTML = 'Vui lòng nhập số điện thoại';
-        }else if (phone.trim().length > 10) {
-            phone = '';
-            document.getElementById('phone-error').innerHTML = 'Số điện thoại không đúng';
-        }else {
-            document.getElementById('phone-error').innerHTML = '';
-        }
+    if (_.isEmpty(phone)) {
+        phone = '';
+        document.getElementById('phone-error').innerHTML = 'Vui lòng nhập số điện thoại';
+    }else if (phone.trim().length > 10) {
+        phone = '';
+        document.getElementById('phone-error').innerHTML = 'Số điện thoại không đúng';
+    }else {
+        document.getElementById('phone-error').innerHTML = '';
+    }
 
-        if (_.isEmpty(address)) {
-            address = '';
-            document.getElementById('address-error').innerHTML = 'Vui lòng nhập địa chỉ';
-        }else {
-            document.getElementById('address-error').innerHTML = '';
-        }
+    if (_.isEmpty(address)) {
+        address = '';
+        document.getElementById('address-error').innerHTML = 'Vui lòng nhập địa chỉ';
+    }else {
+        document.getElementById('address-error').innerHTML = '';
+    }
 
-        if (_.isEmpty(gender)) {
-            gender = '';
-            document.getElementById('gender-error').innerHTML = 'Vui lòng chọn giới tính';
-        }else {
-            document.getElementById('gender-error').innerHTML = '';
-        }
+    if (_.isEmpty(gender)) {
+        gender = '';
+        document.getElementById('gender-error').innerHTML = 'Vui lòng chọn giới tính';
+    }else {
+        document.getElementById('gender-error').innerHTML = '';
+    }
 
-        if (fullname && email && phone && address && gender) {
-            let staffs = localStorage.getItem('staffs') ? JSON.parse(localStorage.getItem('staffs')) : [];
+    if (fullname && email && phone && address && gender) {
+        let staffs = localStorage.getItem('staffs') ? JSON.parse(localStorage.getItem('staffs')) : [];
 
+        if (currentEditingStaffIndex !== null) {
+            // Đang chỉnh sửa
+            staffs[currentEditingStaffIndex] = {
+                fullname: fullname,
+                email: email,
+                phone: phone,
+                address: address,
+                gender: gender,
+            };
+            currentEditingStaffIndex = null;
+            document.getElementById('save').style.display = 'inline-block';
+            document.getElementById('update').style.display = 'none';
+        } else {
+            // Thêm mới
             staffs.push({
                 fullname: fullname,
                 email: email,
                 phone: phone,
                 address: address,
                 gender: gender,
-        }); 
+            }); 
+        }
 
-        localStorage.setItem('staffs', JSON.stringify((staffs)));
-        this.showListStaff ();
+        localStorage.setItem('staffs', JSON.stringify(staffs));
+        showListStaff();
     }
 }
-
 function showListStaff() {
     let staffs = localStorage.getItem('staffs') ? JSON.parse(localStorage.getItem('staffs')) : [];
     
@@ -129,51 +145,17 @@ function deleteStaff(id) {
 
 function editStaff(id) {
     let staffs = localStorage.getItem('staffs') ? JSON.parse(localStorage.getItem('staffs')) : [];
-    let staff = staffs[id];
-
-    // Đổ dữ liệu của nhân viên vào các trường nhập liệu
-    document.getElementById('fullname').value = staff.fullname;
-    document.getElementById('email').value = staff.email;
-    document.getElementById('phone').value = staff.phone;
-    document.getElementById('address').value = staff.address;
-
-    // Chọn giới tính tương ứng
-    if (staff.gender === '1') {
+    currentEditingStaffIndex = id;
+    document.getElementById('fullname').value = staffs[id].fullname;
+    document.getElementById('email').value = staffs[id].email;
+    document.getElementById('phone').value = staffs[id].phone;
+    document.getElementById('address').value = staffs[id].address;
+    if (staffs[id].gender === '1') {
         document.getElementById('male').checked = true;
     } else {
         document.getElementById('female').checked = true;
     }
-
-    // Hiển thị form sửa thông tin và ẩn form thêm mới
-    document.getElementById('add-form').style.display = 'none';
-    document.getElementById('edit-form').style.display = 'block';
-
-    // Lưu lại vị trí của nhân viên đang được chỉnh sửa để sử dụng trong hàm updateStaff()
-    document.getElementById('edit-staff-id').value = id;
+    document.getElementById('save').style.display = 'none';
+    document.getElementById('update').style.display = 'inline-block';
 }
 
-function updateStaff() {
-    let id = document.getElementById('edit-staff-id').value;
-    let staffs = localStorage.getItem('staffs') ? JSON.parse(localStorage.getItem('staffs')) : [];
-    let staff = staffs[id];
-
-    // Cập nhật thông tin của nhân viên
-    staff.fullname = document.getElementById('fullname').value;
-    staff.email = document.getElementById('email').value;
-    staff.phone = document.getElementById('phone').value;
-    staff.address = document.getElementById('address').value;
-    staff.gender = document.querySelector('input[name="gender"]:checked').value;
-
-    // Lưu lại vào localStorage
-    localStorage.setItem('staffs', JSON.stringify(staffs));
-
-    // Hiển thị lại danh sách nhân viên
-    showListStaff();
-
-    // Ẩn form sửa thông tin và hiện form thêm mới
-    document.getElementById('edit-form').style.display = 'none';
-    document.getElementById('add-form').style.display = 'block';
-
-    // Xóa giá trị của input lưu vị trí nhân viên đang được chỉnh sửa
-    document.getElementById('edit-staff-id').value = '';
-}
